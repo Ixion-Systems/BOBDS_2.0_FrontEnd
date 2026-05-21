ef71ce01009a0779468eb9985b76c7ec893d5622
NicoWASD <nicolasneilpereira@gmail.com>
2026-05-21 20:02:09 -0300

    Merge branch 'main' of https://github.com/Ixion-Systems/Bob-Do-Something-2.0

---
+++ bobserver/src/test/java/com/bobds/bobserver/BobserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bobds.bobserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BobserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
