f10a3bfbca0568131d0663d08ac9891965a275ec
juansesinho <juanse.sfalcon@gmail.com>
2026-05-21 15:29:15 -0300

    spring boot

---
+++ bobserver/src/main/java/com/bobds/bobserver/BobserverApplication.java
@@ -0,0 +1,13 @@
+package com.bobds.bobserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BobserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BobserverApplication.class, args);
+	}
+
+}
